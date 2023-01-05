2ace9ddf47ee92ea6dcf904c4d129424d93c14ac
CBO <quddhr9523@naver.com>
2023-01-05 19:52:27 +0900

    링크드 리스트 수업 -2-

--- src/DataStructure/SingleLinkedList.java
+++ src/DataStructure/SingleLinkedList.java
@@ -3,6 +3,7 @@ package DataStructure;
 /**
  * 링크드 리스트 직접 구현해보기
  * 리스트의 마지막에 데이터 추가 하는것만 해봄
+ * 리스트를 출력하는것 해봄
  */
 public class SingleLinkedList<T> {
     public Node<T> head = null;
@@ -26,13 +27,26 @@ public class SingleLinkedList<T> {
             node.next = new Node<T>(data);
         }
     }
+    public void printAll() {
+        if(head == null){
+            System.out.println("데이터 없음");
+        }else {
+            Node<T> node = this.head;
+            while (node != null){
+                System.out.print(node.data + " ");
+                node = node.next;
+            }
+        }
+    }
 
     public static void main(String[] args) {
         SingleLinkedList<Integer> MyLinkedList = new SingleLinkedList<>();
         MyLinkedList.addNode(1);
         MyLinkedList.addNode(2);
 
-        System.out.println(MyLinkedList.head.data);
-        System.out.println(MyLinkedList.head.next.data);
+//        System.out.println(MyLinkedList.head.data);
+//        System.out.println(MyLinkedList.head.next.data);
+
+        MyLinkedList.printAll();
     }
 }
