ae2ac7dd6456fa3d16de5a0e8754a2c402ab77f4
CBO <quddhr9523@naver.com>
2023-06-01 21:30:12 +0900

    [3차] 파일명 정렬(실패) 정답률 80% 테스트 케이스 6,7,8,9에서 실패

--- src/Programmers2/Solution48.java
+++ src/Programmers2/Solution48.java
@@ -3,9 +3,10 @@ package Programmers2;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 /**
- *[3차] 파일명 정렬(실패) 정답률 50%
+ *[3차] 파일명 정렬(실패) 정답률 80%
  */
 public class Solution48 {
      class FileName {
@@ -57,30 +58,30 @@ public class Solution48 {
      }
 
     public String[] solution(String[] files) {
-        String[] answer = new String[files.length];
         List<FileName> list = new ArrayList<>();
         for (String file : files) {
             int head = 0;
             int number = 0;
             for (int i = 0; i < file.length(); i++) {
-                if (file.charAt(i) >= 48 && file.charAt(i) <= 57) {
+                if (file.charAt(i) >= '0' && file.charAt(i) <= '9') {
                     head = i;
                     break;
                 }
             }
+            int count = 0;
+
             for (int i = head; i < file.length(); i++) {
-                if (!(file.charAt(i) >= 48 && file.charAt(i) <= 57)) {
+                if (!(file.charAt(i) >= '0' && file.charAt(i) <= '9') || count > 4) {
                     number = i;
                     break;
+                } else {
+                    count++;
                 }
             }
-            if (!file.contains(".")) {
-                String header = file.substring(0, head);
-                String num = file.substring(head);
-                FileName fileName = new FileName(header, num, "");
-                list.add(fileName);
-                continue;
+            if (number == 0) {
+                number = file.length();
             }
+
             String header = file.substring(0, head);
             String num = file.substring(head, number);
             String tail = file.substring(number);
@@ -92,19 +93,26 @@ public class Solution48 {
             if (o1.getHead().equalsIgnoreCase(o2.getHead())) {
                 return Integer.parseInt(o1.getNumber()) - Integer.parseInt(o2.getNumber());
             } else {
-                return o1.getHead().compareTo(o2.getHead());
+                return o1.getHead()
+                        .toUpperCase()
+                        .replaceAll("[^A-Z]", "")
+                        .compareToIgnoreCase(o2.getHead()
+                                .toUpperCase()
+                                .replaceAll("[^A-Z]", ""));
             }
         });
-        for (int i = 0; i < answer.length; i++) {
-            answer[i] = list.get(i).perfectFileName();
-        }
-        return answer;
+        return IntStream
+                .range(0, files.length)
+                .mapToObj(i -> list.get(i).perfectFileName())
+                .toArray(String[]::new);
     }
 
     public static void main(String[] args) {
         String[] files1 = {"img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"};
-        String[] files2 = {"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"};
+        String[] files2 = {"F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat", "F-15"};
+        String[] files3 = {"-F-15f42345", "F-15342345", "g341fdsaf.jpg", "H g000342.jpg", "H g00 1342.jpg", "f- g 345.jpg"};
+        String[] files4 = {"F-15"};
 
-        System.out.println(Arrays.toString(new Solution48().solution(files2)));
+        System.out.println(Arrays.toString(new Solution48().solution(files4)));
     }
 }
