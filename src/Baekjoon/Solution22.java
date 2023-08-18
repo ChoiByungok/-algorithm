3fb3e79e981d1caee2cc4ba8604831b314d23add
CBO <quddhr9523@naver.com>
2023-08-18 12:59:10 +0900

    1927 최소 힙

---
+++ src/Baekjoon/Solution22.java
@@ -0,0 +1,31 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+/**
+ * 1927 최소 힙
+ */
+public class Solution22 {
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        StringBuilder stringBuilder = new StringBuilder();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int repeat = Integer.parseInt(bufferedReader.readLine());
+        for (int i = 0; i < repeat; i++) {
+            String readLine = bufferedReader.readLine();
+            if (readLine.equals("0")) {
+                if (queue.isEmpty()) {
+                    stringBuilder.append("0").append("\n");
+                } else {
+                    stringBuilder.append(queue.poll()).append("\n");
+                }
+            } else {
+                queue.add(Integer.parseInt(readLine));
+            }
+        }
+        System.out.println(stringBuilder);
+    }
+}
